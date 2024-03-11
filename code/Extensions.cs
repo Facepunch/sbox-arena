79c2476a69f623ba2a05f91bd9e23d92d1db0a1b
kurozael <kurozael@gmail.com>
2024-03-11 15:00:30 +0000

    Update for clothing api

--- code/Extensions.cs
+++ code/Extensions.cs
@@ -29,42 +29,42 @@ public static class Extensions
 	{
 		self.Reset( body );
 		
-		var skinMaterial = self.Clothing?.Select( x => x?.SkinMaterial ).Where( x => !string.IsNullOrWhiteSpace( x ) ).Select( Material.Load ).FirstOrDefault();
-		var eyesMaterial = self.Clothing?.Select( x => x?.EyesMaterial ).Where( x => !string.IsNullOrWhiteSpace( x ) ).Select( Material.Load ).FirstOrDefault();
+		var skinMaterial = self.Clothing?.Select( x => x?.Clothing.SkinMaterial ).Where( x => !string.IsNullOrWhiteSpace( x ) ).Select( Material.Load ).FirstOrDefault();
+		var eyesMaterial = self.Clothing?.Select( x => x?.Clothing.EyesMaterial ).Where( x => !string.IsNullOrWhiteSpace( x ) ).Select( Material.Load ).FirstOrDefault();
 
 		if ( skinMaterial is not null ) body.SetMaterialOverride( skinMaterial, "skin" );
 		if ( eyesMaterial is not null ) body.SetMaterialOverride( eyesMaterial, "eyes" );
 		
 		foreach ( var c in self.Clothing )
 		{
-			var modelPath = c.GetModel( self.Clothing.Except( new[] { c } ) );
+			var modelPath = c.Clothing.GetModel( self.Clothing.Except( new[] { c } ).Select( v => v.Clothing ) );
 
-			if ( string.IsNullOrEmpty( modelPath ) || !string.IsNullOrEmpty( c.SkinMaterial ) )
+			if ( string.IsNullOrEmpty( modelPath ) || !string.IsNullOrEmpty( c.Clothing.SkinMaterial ) )
 				continue;
 
 			var model = Model.Load( modelPath );
 			if ( model is null || model.IsError )
 				continue;
 
-			var go = new GameObject( false, $"Clothing - {c.ResourceName}" )
+			var go = new GameObject( false, $"Clothing - {c.Clothing.ResourceName}" )
 			{
 				Parent = body.GameObject
 			};
 
 			var component = go.Components.Create<ClothingComponent>();
-			component.Category = c.Category;
+			component.Category = c.Clothing.Category;
 			
 			go.Tags.Add( "clothing" );
 			
 			var r = go.Components.Create<SkinnedModelRenderer>();
-			r.Model = Model.Load( c.Model );
+			r.Model = Model.Load( c.Clothing.Model );
 			r.BoneMergeTarget = body;
 
 			if ( skinMaterial is not null ) r.SetMaterialOverride( skinMaterial, "skin" );
 			if ( eyesMaterial is not null ) r.SetMaterialOverride( eyesMaterial, "eyes" );
 
-			if ( !string.IsNullOrEmpty( c.MaterialGroup ) )
-				r.MaterialGroup = c.MaterialGroup;
+			if ( !string.IsNullOrEmpty( c.Clothing.MaterialGroup ) )
+				r.MaterialGroup = c.Clothing.MaterialGroup;
 			
 			go.Enabled = true;
 		}
